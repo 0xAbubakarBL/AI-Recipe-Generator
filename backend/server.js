00faeeef52446ff8415d0403353f445e92f4f847
0xAbubakarBL <0xabubakarbl@gmail.com>
2026-06-03 14:40:45 +0100

    vercel ready

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,9 @@ import shoppingListRoutes from './routes/shoppingList.js';
 const app = express();
 
 //Middleware
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
